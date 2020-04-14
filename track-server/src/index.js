5ae1b8676d489f37edb8079901fbcf925f5b207e
Richard Aldrich <rsie0001@student.monash.edu>
2020-04-14 18:52:15 +1000

    Create simple sign up process

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,27 @@
+require('./models/Users');
+
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0-1hvbs.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance.');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo',  err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi there!');
---
+++ track-server/src/routes/authRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+    // console.log(req.body);
+    const { email, password } = req.body;
+    try {
+        const user = new User({ email, password });
+        await user.save();
+
+        res.send('You made a post request');
+    } catch (err) {
+        res.status(422).send(err.message);
+    }
+});
+
+module.exports = router;
