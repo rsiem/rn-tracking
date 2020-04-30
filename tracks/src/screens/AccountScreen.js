aef5032a0447b37c1f51bc96fae12089db60eff3
Richard Aldrich <rsie0001@student.monash.edu>
2020-04-30 17:26:16 +1000

    signing out a user
    signing out a user and make content below the status bar

--- tracks/src/screens/AccountScreen.js
+++ tracks/src/screens/AccountScreen.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Button } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const AccountScreen = () => {
+    const { signout } = useContext(AuthContext);
+
     return (
-        <>
+        <SafeAreaView forceInset={{ top: 'always' }}>
             <Text>Account Screen</Text>
-        </>
+            <Spacer>
+                <Button title="Sign Out" onPress={signout} />
+            </Spacer>
+        </SafeAreaView>
     );
 };
 
--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
 
 const TrackCreateScreen = () => {
     return (
-        <>
+        <SafeAreaView forceInset={{ top: 'always' }}>
             <Text>Track Create Screen</Text>
-        </>
+        </SafeAreaView>
     );
 };
 
